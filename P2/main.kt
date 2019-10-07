f175e8d95a8313d12272c56288f97c07a81d1ec4
iinow <iinow@naver.com>
2019-10-07 21:26:21 +0900

    이름 변경

--- P2/main.kt
+++ P2/main.kt
@@ -2,9 +2,9 @@ fun add(value: Int): Int = value
 
 fun Int.add(value: Int): Int = this + value
 
-fun Int.substract(value: Int): Int = this - value
+fun Int.subtract(value: Int): Int = this - value
 
 fun main(args: Array<String>) {
-    val result = add(4).add(5).substract(3)
+    val result = add(4).add(5).subtract(3)
     println(result)
 }
